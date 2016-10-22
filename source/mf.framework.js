9fb7cef5a7836c290f68f317b034dad57c0cbab7
mosesfender <mosesfender@gmail.com>
2016-10-22 22:42:45 +0300

    Update mf.framework.js

--- source/mf.framework.js
+++ source/mf.framework.js
@@ -113,6 +113,15 @@ mf = {
         window.CustomEvent = CustomEvent;
     }
     ;
+    
+        window.batchAddListener = function (elements, eventName, callback) {
+        var events = eventName.split(',');
+        for (var i = 0; i < elements.length; i++) {
+            for (var e = 0; e < events.length; e++) {
+                elements[i].addEventListener(events[e].trim(), callback);
+            }
+        }
+    };
 
     window.sprintf = function () {
         //  discuss at: http://phpjs.org/functions/sprintf/
